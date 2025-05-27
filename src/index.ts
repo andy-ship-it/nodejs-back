96eebf7c50b1e9647f4ac466a56e9d23d0d411e9
Andy Richard Centeno Gutierez <andycgutierrz70@gmail.com>
2025-05-27 03:39:26 -0500

    Arreglar conflicto y actualizar index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
-feature/NSTI-9
 import server from "./server";
 const port =3000;
 server.listen(port, () => {
-
-import express from 'express';
-
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hola a SENATI!');
-});
-
-const port =3000;
-app.listen(port, () => {
-develop
     console.log('Server running on port', port);
 });
