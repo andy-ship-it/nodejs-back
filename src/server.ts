8d7a26319a91ab7d9184b864929c63591ac1f2bb
Andy Richard Centeno Gutierez <andycgutierrz70@gmail.com>
2025-05-27 03:22:13 -0500

    perf: NJTI-9 Configurar el servidor en archivo configuracion

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,4 @@ app.get('/', (req, res) => {
     res.send('Hola a SENATI!');
 });
 
-const port =3000;
-app.listen(port, () => {
-    console.log('Server running on port', port);
-});
+export default app;
